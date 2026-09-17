85c4e570da092503116c2986cf737f75d594dfb3
Nickolas <nickolasram@gmail.com>
2026-09-16 23:49:51 -0700

    article preview finish for now

--- app/(public)/demos/articles/page.tsx
+++ app/(public)/demos/articles/page.tsx
@@ -4,6 +4,14 @@ import {PushDynamoArticle} from "@/classes";
 import SimpleVertical from "@/app/components/frameworks/simpleVertical";
 import {Suspense} from "react";
 import PushArticlePreview from "@/components/pushArticlePreview";
+import {listAndKVReference} from "@/types";
+
+const KVs:listAndKVReference[] = [
+    {
+        display: 'Admin',
+        value: '1245a',
+    }
+]
 
 export default async function Page(){
     const articles = await PushDynamoArticle.getAllPublishedArticles()
@@ -11,11 +19,13 @@ export default async function Page(){
         <SimpleVertical>
             <h1>Articles Demo</h1>
             <Suspense fallback={<p>Loading...</p>}>
-                { articles.map((article,i) => (
+                { articles.filter(obj=>{return obj.published}).map((article,i) => (
                   <PushArticlePreview
                       article={article.plainObject()}
                       key={i}
                       style={'neon'}
+                      showAuthor={true}
+                      authorsReference={KVs}
                   />
                 ))
                 }
--- components/pushArticlePreview.tsx
+++ components/pushArticlePreview.tsx
@@ -1,15 +1,56 @@
-import {PushArticleSummary} from "@/types";
+import {listAndKVReference, PushArticleSummary} from "@/types";
 
 interface papProps{
     article:PushArticleSummary;
     rounded?:'rounded-xs'|'rounded-sm'|'rounded-md'|'rounded-lg'|'rounded-xl'|string;
     style?:'neon'|'marbleBtn'|string;
+    showAuthor?: boolean;
+    authorsReference?:listAndKVReference[];
 }
 
-const PushArticlePreview=({article,rounded,style}:papProps)=>{
+const PushArticlePreview=({article,rounded,style,showAuthor,authorsReference}:papProps)=>{
+    const visibleContributors = article.contributors.filter(obj=>(!obj.hidden))
+    const contributorAlias = visibleContributors.map(obj=>{
+        if (obj.object){
+            return (
+                (authorsReference??[]).find(ref=>{
+                    return(ref.value==obj.value)
+                })?.display??'[user not found]'
+            )
+        } else {
+            return obj.value
+        }
+    })
+    const sortedContributors = contributorAlias.sort((objA,objB)=>{return objA.localeCompare(objB)});
+    const contributorString = sortedContributors.join(', ');
+    let timestamp;
+    if (article.firstPublishedDate==article.latestUpdatedDate){
+        timestamp = 'Posted'
+    } else {
+        timestamp = 'Updated'
+    }
     return (
         <div className={`w-full ${style=='neon'?'border-6 border-neon-cyan text-black bg-white':'border'} ${rounded??'rounded-xs'} p-3`}>
-            <p className={'font-semibold'}>{article.heading}</p>
+            <div className={'w-full flex justify-between'}>
+                <p className={'font-semibold text-2xl'}>{article.heading}</p>
+                <div>
+                    <p>{timestamp}</p>
+                    <p>
+                        {new Date((article.latestUpdatedDate??article.firstPublishedDate)??'').toLocaleString(undefined, {
+                                year: '2-digit',
+                                month:'numeric',
+                                day:'numeric',
+                                hour:'numeric',
+                                minute:'numeric',
+                                })}
+                    </p>
+                </div>
+            </div>
+            { !!showAuthor &&
+                <div>
+                    <p>By {contributorString}</p>
+                </div>
+            }
         </div>
     )
 }
